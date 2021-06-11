77c7f2900d83be08dce8ebabd2ac960c72ad52d2
Jing-Cheng, Wu <wolf952@gmail.com>
2021-06-11 07:06:43 +0000

    add prism package

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,33 @@
 import { Link as GatsbyLink } from "gatsby";
 import React from "react";
-import { Box, Heading, Link } from "theme-ui";
+import { Box, Button, Flex, Heading, Link, useColorMode } from "theme-ui";
 
 export const Header = ({ siteTitle, siteDescription }) => {
+  const [colorMode, setColorMode] = useColorMode();
   return (
     <Box as="header" sx={{ bg: "highlight", mb: "1.45rem" }}>
-      <Box
-        as="div"
+      <Box as="div"
         sx={{
           m: "0 auto",
           maxWidth: "640px",
           p: "1.45rem 1.0875rem",
         }}
       >
-        <Link as={GatsbyLink} to="/">
-          <Heading>{siteTitle}</Heading>
-        </Link>
-        <Box as="p" variant="styles.p">
-          {siteDescription}
-        </Box>
+        <Flex>
+          <Box sx={{ flex: "1 1 auto", flexDirection: "column" }}>
+            <Link as={GatsbyLink} to="/">
+              <Heading>{siteTitle}</Heading>
+            </Link>
+          </Box>
+          <Box as="p" variant="styles.p">
+            {siteDescription}
+          </Box>
+          <Button
+            onClick={(e) => {setColorMode(colorMode === "default" ? "dark" : "default");}}
+          >
+            {colorMode === "default" ? "Dark" : "Light"}
+          </Button>
+        </Flex>
       </Box>
     </Box>
   );

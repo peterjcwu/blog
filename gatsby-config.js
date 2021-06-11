93e5a9888d44b731ece4d81ca269a4e470e19be3
Jing-Cheng, Wu <wolf952@gmail.com>
2021-06-11 08:07:13 +0000

    add config image

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,26 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-plugin-mdx",
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 640,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {

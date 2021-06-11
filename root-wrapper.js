d7f464f392843372d6cd7f807d4171226b41b0cf
Jing-Cheng, Wu <wolf952@gmail.com>
2021-06-11 07:15:35 +0000

    add component for mdx

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,7 +1,17 @@
 // root-wrapper.js
 import React from "react";
 import { Layout } from "./src/components/layout";
+import { MDXProvider } from "@mdx-js/react";
+import { RainbowText } from "./src/components/rainbow-text";
+
+const MDXComponents = {
+  RainbowText,
+};
 
 export const rootWrapper = ({ element }) => {
-  return <Layout>{element}</Layout>;
+  return (
+    <Layout>
+      <MDXProvider components={MDXComponents}>{element}</MDXProvider>
+    </Layout>
+  );
 };
